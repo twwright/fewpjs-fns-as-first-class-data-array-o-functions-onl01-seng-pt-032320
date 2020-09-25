9a04ae068cef9deddb3d14a49e1c4a585b3bf504
Thomas Wright <twwright@gmail.com>
2020-09-25 17:16:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const DoggyWalk = {
-  
-}
+let dog = `${dogName} the ${dogBreed}`;
+
+wakeDog = (dogName, dogBreed) => console.log(`Wake ${dog}`);
+leashDog = (dogName, dogBreed) => console.log(`Leash ${dog}`);
+walkToPark = (dogName, dogBreed) => console.log(`Walk to the park with ${dog}`);
+throwFrisbee = (dogName, dogBreed) => console.log(`Throw the frisbee for ${dog}`);
+walkHome = (dogName, dogBreed) => console.log(`Walk home with ${dog}`);
+unleashDog = (dogName, dogBreed) => console.log(`Unleash ${dog}`);
+

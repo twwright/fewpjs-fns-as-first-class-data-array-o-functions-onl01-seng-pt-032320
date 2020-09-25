d0479723f7b87b815603b474764507b9b2345106
Thomas Wright <twwright@gmail.com>
2020-09-25 17:18:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,13 @@ throwFrisbee = (dogName, dogBreed) => console.log(`Throw the frisbee for ${dog}`
 walkHome = (dogName, dogBreed) => console.log(`Walk home with ${dog}`);
 unleashDog = (dogName, dogBreed) => console.log(`Unleash ${dog}`);
 
+const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
 
+exerciseDog = (dogName, dogBreed) => {
+  const executedRoutine = [];
+  for (const action in array) {
+    executedRoutine.push(action(dogName, dogBreed));
+  }
+  return executedRoutine;
+}
 

36aea48e08762f14d3ba078f38d4993aad84b40b
Thomas Wright <twwright@gmail.com>
2020-09-25 17:21:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let dog = `${dogName} the ${dogBreed}`;
 
-wakeDog = (dogName, dogBreed) => console.log(`Wake ${dog}`);
-leashDog = (dogName, dogBreed) => console.log(`Leash ${dog}`);
-walkToPark = (dogName, dogBreed) => console.log(`Walk to the park with ${dog}`);
-throwFrisbee = (dogName, dogBreed) => console.log(`Throw the frisbee for ${dog}`);
-walkHome = (dogName, dogBreed) => console.log(`Walk home with ${dog}`);
-unleashDog = (dogName, dogBreed) => console.log(`Unleash ${dog}`);
+const wakeDog = (dogName, dogBreed) => console.log(`Wake ${dog}`);
+const leashDog = (dogName, dogBreed) => console.log(`Leash ${dog}`);
+const walkToPark = (dogName, dogBreed) => console.log(`Walk to the park with ${dog}`);
+const throwFrisbee = (dogName, dogBreed) => console.log(`Throw the frisbee for ${dog}`);
+const walkHome = (dogName, dogBreed) => console.log(`Walk home with ${dog}`);
+const unleashDog = (dogName, dogBreed) => console.log(`Unleash ${dog}`);
 
 const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
 
-exerciseDog = (dogName, dogBreed) => {
+const exerciseDog = (dogName, dogBreed) => {
   return routine.map(action => action(dogName, dogBreed))
 }

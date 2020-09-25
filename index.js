65855723a6e4337986312ef269519a947c400312
Thomas Wright <twwright@gmail.com>
2020-09-25 17:53:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-wakeDog = function(dogName, dogBreed) { 
+const wakeDog = function(dogName, dogBreed) { 
   let dog = `${dogName} the ${dogBreed}`;
   console.log(`Wake ${dog}`);
   return `Wake ${dog}`;
 }
 
-leashDog = function(dogName, dogBreed) {
+const leashDog = function(dogName, dogBreed) {
   let dog = `${dogName} the ${dogBreed}`;
   console.log(`Leash ${dog}`);
   return `Unleash ${dog}`;
 }
 
-walkToPark = function(dogName, dogBreed) {
+const walkToPark = function(dogName, dogBreed) {
   let dog = `${dogName} the ${dogBreed}`;
   console.log(`Walk to the park with ${dog}`);
   return `Walk to the park with ${dog}`;
 }
   
   
-throwFrisbee = function(dogName, dogBreed) {
+const throwFrisbee = function(dogName, dogBreed) {
   let dog = `${dogName} the ${dogBreed}`;
   console.log(`Throw the frisbee for ${dog}`);
   return `Throw the frisbee for ${dog}`;
 }
 
-walkHome = function(dogName, dogBreed) {
+const walkHome = function(dogName, dogBreed) {
   let dog = `${dogName} the ${dogBreed}`;
   console.log(`Walk home with ${dog}`);
   return `Walk home with ${dog}`;
 }
 
-unleashDog = function(dogName, dogBreed) {
+const unleashDog = function(dogName, dogBreed) {
   let dog = `${dogName} the ${dogBreed}`;
   console.log(`Unleash ${dog}`);
   return `Unleash ${dog}`;
